72a6210077ad0656a3184cdd56d23bffb6af3d8a
Rizki Maulana <rizki.maulana23@ui.ac.id>
2025-06-01 22:57:02 +0700

    remove unnecessary import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import { theme } from './theme';
